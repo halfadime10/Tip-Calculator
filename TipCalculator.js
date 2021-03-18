48524f8c8fff13c1df8fdd25c2131722a9af9413
Nicole Grassi <nicolegrassi13@gmail.com>
2021-03-18 14:52:39 -0700

    Add files via upload

--- TipCalculator.js
+++ TipCalculator.js
@@ -10,9 +10,23 @@ window.onload = function() {
 
     let tip = (totalBillAmount * totalTipPercentage) / partySize;
 
-    let tip_el = document.querySelector('.tip-wrap .tip-amount');
+    let tip_el = document.querySelector('#tip-amount');
     tip_el.innerText = '$' + Math.round(tip*100)/100 + ((partySize > 1) ? ' each' : '');
+    return tip;
   }
-  document.getElementById("tip-btn").addEventListener("click", calc_tip);
+
+  function calc_split() {
+    let totalBillAmount = Number(billAmountInput.value);
+    let partySize = Number(partySizeInput.value);
+    let tip = calc_tip();
+
+    let split = (totalBillAmount / partySize) + tip;
+
+    let split_el = document.querySelector('#split-amount');
+    split_el.innerText = '$' + Math.round(split*100)/100 + ((partySize > 1) ? ' each' : '');
+  }
+
+  document.getElementById("calc-btn").addEventListener("click", calc_tip);
+  document.getElementById("calc-btn").addEventListener("click", calc_split);
 
 };
